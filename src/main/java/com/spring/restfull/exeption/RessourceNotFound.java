95913737a22665c785dbcb9926a4d2644bd6ee7f
Leo <leodelmas31@gmail.com>
2024-09-08 16:26:04 +0200

    ressource not found

--- src/main/java/com/spring/restfull/exeption/RessourceNotFound.java
+++ src/main/java/com/spring/restfull/exeption/RessourceNotFound.java
@@ -1,5 +1,8 @@
 package com.spring.restfull.exeption;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus
 public class RessourceNotFound extends RuntimeException {
 
 	/**
